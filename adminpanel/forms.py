3b45d08d6c784385a2ef20dc44569cfc0adbae01
Dany1122 <danitza.an.ch@gmail.com>
2025-06-25 20:39:05 -0600

    modulo de servicios

--- adminpanel/forms.py
+++ adminpanel/forms.py
@@ -1,7 +1,27 @@
 from django import forms
 from usuarios.models import CustomUser
+from servicios.models import Servicio
 
 class CustomUserForm(forms.ModelForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'rol', 'is_active', 'is_staff']
+        fields = ['username', 
+                  'email', 
+                  'rol', 
+                  'is_active', 
+                  'is_staff']
+
+class ServicioForm(forms.ModelForm):
+    class Meta:
+        model = Servicio
+        fields = ['titulo', 
+                  'servicio', 
+                  'descripcion', 
+                  'costo_por_persona',
+                  'costo_niño', 
+                  'costo_con_descuento', 
+                  'imagen_principal',
+                  'duracion', 
+                  'restricciones', 
+                  'galeria'
+        ]
--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -3,10 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='admin_dashboard'),
+    
     path('usuarios/', views.lista_usuarios, name='admin_usuarios'),
     path('usuarios/editar/<int:id>/', views.editar_usuario, name='admin_editar_usuario'),
     path('usuarios/eliminar/<int:id>/', views.eliminar_usuario, name='admin_eliminar_usuario'),
     path('usuarios/agregar/', views.agregar_usuario, name='admin_agregar_usuario'),
-     path("usuarios/exportar/pdf/", views.exportar_usuarios_pdf,
-         name="admin_exportar_usuarios_pdf"),
+    path("usuarios/exportar/pdf/", views.exportar_usuarios_pdf, name="admin_exportar_usuarios_pdf"),
+    
+    path('servicios/', views.lista_servicios, name='admin_servicios'),
+    path('servicios/agregar', views.agregar_servicio, name='admin_agregar_servicio'),
+    path('servicios/editar/<int:id>/', views.editar_servicio, name='admin_editar_servicio'),
+    path('servicios/eliminar/<int:id>/', views.eliminar_servicio, name='admin_eliminar_servicio'),
+    path("servicios/exportar/pdf/", views.exportar_servicios_pdf, name="admin_exportar_servicios_pdf"),
+    
 ]
